bc2895aeb880fbb8d30950f8fc96c7abe39e23b2
Joss-Steward <joss.steward@gmail.com>
2015-08-26 22:18:45 -0400

    Made a chart

--- WebInterface/Controllers/DefaultController.cs
+++ WebInterface/Controllers/DefaultController.cs
@@ -24,5 +24,40 @@ namespace WebInterface.Controllers
                 return View(samples);
             }
         }
+
+        public ActionResult Chart()
+        {
+            return View();
+        }
+
+        public ActionResult ChartData()
+        {
+            using (var context = new WaterContext())
+            {
+                var data = context.Stations.Select(s => new
+                {
+                    label = s.Name,
+                    data = s.Samples.Take(50).Select(d => new
+                    {
+                        time = d.DateTime,
+                        oxy = d.Oxygen
+                    })
+                }).ToList();
+
+                /* Time in JavaScript is stored as milliseconds ellapsed since the Unix epoch */
+                var epochTicks = new DateTime(1970, 1, 1).Ticks;
+
+                var json = data.Select(s => new
+                {
+                    label = s.label,
+                    data = s.data.Select(p => new double[]{
+                        ((p.time.Ticks - epochTicks) / TimeSpan.TicksPerMillisecond),
+                        (double)p.oxy
+                    })
+                });
+
+                return Json(json, JsonRequestBehavior.AllowGet);
+            }
+        }
     }
 }
